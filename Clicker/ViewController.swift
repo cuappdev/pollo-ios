de78bc3b68c575f29dc6f0f7ed3376804859bf8e
Daniel Li <dl743@cornell.edu>
2017-02-12 14:18:04 -0500

    Add project files

---
+++ Clicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clicker
+//
+//  Created by Daniel Li on 2/12/17.
+//  Copyright © 2017 cuappdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
