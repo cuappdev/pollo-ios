a4f6a4c5421c3e25cc7f0d00c010e8e84a540f44
Keivan Shahida <keivanshahida@icloud.com>
2017-10-01 13:01:57 -0400

    Basic headers and cells set up

--- Clicker/View Controllers/HomeViewController.swift
+++ Clicker/View Controllers/HomeViewController.swift
@@ -10,22 +10,72 @@ import UIKit
 
 class HomeViewController: UITableViewController {
 
+    // MARK: - INITIALIZATION
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .blue // TEMP
+        self.title = "CliquePod"
+        
+        tableView.separatorStyle = .none
+        tableView.bounces = false
+        
+        tableView.register(LiveSessionHeader.self, forHeaderFooterViewReuseIdentifier: "liveSessionHeader")
+        tableView.register(PastSessionHeader.self, forHeaderFooterViewReuseIdentifier: "pastSessionHeader")
+        tableView.register(LiveSessionTableViewCell.self, forCellReuseIdentifier: "liveSessionCell")
+        tableView.register(PastSessionTableViewCell.self, forCellReuseIdentifier: "pastSessionCell")
     }
 
-
-    // MARK: - TableView DataSource
-
+    // MARK: - CELLS
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return PastSessionTableViewCell() // TEMP
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    
+    // MARK: - SECTIONS
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
-
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        var headerView = UITableViewHeaderFooterView()
+        switch (section) {
+        case 0:
+            headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "liveSessionHeader") as! LiveSessionHeader
+            return headerView
+        case 1:
+            headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "pastSessionHeader") as! PastSessionHeader
+            return headerView
+        default:
+            return headerView
+        }
+    }
+    
+    override  func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch(section){
+        case 0:
+            return 44.5
+        case 1:
+            return 53.5
+        default:
+            return 0
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        switch(section){
+        case 0:
+            return 1
+        case 1:
+            return 1
+        default:
+            return 0
+        }
     }
 }
