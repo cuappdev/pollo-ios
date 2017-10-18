00b6b4de4a8a6bb3e2a45eecbf49a7ade54f78bf
Kevin Chan <kevinchan159@gmail.com>
2017-10-18 00:26:57 -0400

    created and conformed to ClickerQuark protocols

---
+++ Clicker/Quarks/ClickerQuark.swift
@@ -0,0 +1,55 @@
+//
+//  ClickerQuark.swift
+//  Clicker
+//
+//  Created by Kevin Chan on 10/15/17.
+//  Copyright © 2017 CornellAppDev. All rights reserved.
+//
+
+import Neutron
+import SwiftyJSON
+import Alamofire
+
+protocol ClickerQuark: JSONQuark {
+    func process(element: Element) throws -> ResponseType
+}
+
+extension ClickerQuark {
+    public func process(response: JSON) throws -> ResponseType {
+        if let errors = response["errors"].array {
+            let messages = errors.flatMap { $0["message"].string }
+            throw ClickerError.backendError(messages: messages)
+        }
+        
+        if response["node"].exists() {
+            return try process(element: .node(response["node"]))
+        }
+        
+        if let edges = response["edges"].array {
+            let edgesTuples = edges.map {
+                ($0["cursor"].stringValue, $0["node"])
+            }
+            return try process(element: .edges(edgesTuples))
+        }
+        
+        if let errs = response["errors"].array {
+            let messages = errs.map {
+                $0["message"].stringValue
+            }
+            return try process(element: .errors(.backendError(messages: messages)))
+        }
+        throw NeutronError.badResponseData
+    }
+}
+
+typealias Edge = (cursor: String, node: JSON)
+
+enum ClickerError: Error {
+    case backendError(messages: [String])
+}
+
+enum Element {
+    case node(JSON)
+    case edges([Edge])
+    case errors(ClickerError)
+}
