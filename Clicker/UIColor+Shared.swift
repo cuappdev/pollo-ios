3548b7332af997481e2af9fda9765095bcd8de82
Keivan Shahida <keivanshahida@icloud.com>
2017-11-29 14:03:46 -0500

    Added error feedback to AddCourseVC

--- Clicker/UIColor+Shared.swift
+++ Clicker/UIColor+Shared.swift
@@ -11,12 +11,14 @@ import UIKit
 extension UIColor {
     //    UIColor.colorFromCode(0xA23A40).withAlphaComponent(0.26)
     
-    @nonobjc static let clickerBlue = UIColor.colorFromCode(0x01AEF0)
+    @nonobjc static let clickerBlue = UIColor.colorFromCode(0x4EA0FF)
     @nonobjc static let clickerLightGray = UIColor.colorFromCode(0xE8E8E8)
     @nonobjc static let clickerMediumGray = UIColor.colorFromCode(0x969BA6)
     @nonobjc static let clickerDarkGray = UIColor.colorFromCode(0x80868D)
     @nonobjc static let clickerBackground = UIColor.colorFromCode(0xF7F9FA)
     @nonobjc static let clickerBorder = UIColor.colorFromCode(0xE1E1E1)
+    @nonobjc static let clickerRed = UIColor.colorFromCode(0xFF8B8B)
+
 
     public static func colorFromCode(_ code: Int) -> UIColor {
         let red = CGFloat(((code & 0xFF0000) >> 16)) / 255
