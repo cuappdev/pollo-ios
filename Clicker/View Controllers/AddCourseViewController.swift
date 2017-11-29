91729a583bdf0129c19539fe9f60e09d2be20a7b
Keivan Shahida <keivanshahida@icloud.com>
2017-11-29 00:12:13 -0500

    Minor UI fixes. Panning gesture added to enrollment.

--- Clicker/View Controllers/AddCourseViewController.swift
+++ Clicker/View Controllers/AddCourseViewController.swift
@@ -12,31 +12,34 @@ import SnapKit
 
 class AddCourseViewController: UIViewController {
     
-//    var addCourseLabel: UILabel!
+    var addCourseLabel: UILabel!
     var courseTextField: UITextField!
     var addCourseButton: UIButton!
     
+    var touchLocation: CGPoint?
+
     override func viewDidLoad() {
         view.backgroundColor = .white
-        
-        self.title = "Add Course"
-        
         setupSubviews()
+        
+        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGestureAction(_:)))
+        view.addGestureRecognizer(panGestureRecognizer!)
     }
     
     func setupSubviews() {
-//        addCourseLabel = UILabel()
-//        addCourseLabel.text = "Add Course"
-//        addCourseLabel.textColor = .black
-//        addCourseLabel.textAlignment = .center
-//        view.addSubview(addCourseLabel)
+        addCourseLabel = UILabel()
+        addCourseLabel.text = "What is your course code?"
+        addCourseLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        addCourseLabel.textColor = .black
+        addCourseLabel.textAlignment = .left
+        view.addSubview(addCourseLabel)
         
-//        addCourseLabel.snp.makeConstraints { make in
-//            make.centerX.equalToSuperview()
-//            make.centerY.equalToSuperview().offset(-view.frame.height*0.1)
-//            make.width.equalTo(view.frame.width*0.5)
-//            make.height.equalTo(50)
-//        }
+        addCourseLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(100)
+            make.width.equalTo(view.frame.width*0.8)
+            make.height.equalTo(50)
+        }
         
         courseTextField = UITextField()
         courseTextField.backgroundColor = UIColor(red: 236/255, green: 236/255, blue: 236/255, alpha: 1.0)
@@ -47,12 +50,12 @@ class AddCourseViewController: UIViewController {
         courseTextField.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
-            make.width.equalTo(view.frame.width*0.6)
-            make.height.equalTo(40)
+            make.width.equalTo(view.frame.width*0.8)
+            make.height.equalTo(60)
         }
         
         addCourseButton = UIButton()
-        addCourseButton.setTitle("ENROLL", for: .normal)
+        addCourseButton.setTitle("Enroll", for: .normal)
         addCourseButton.titleLabel?.textColor = .white
         addCourseButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
         addCourseButton.backgroundColor = .clickerBlue
@@ -62,9 +65,9 @@ class AddCourseViewController: UIViewController {
 
         addCourseButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.width.equalTo(view.frame.width*0.4)
+            make.width.equalTo(view.frame.width*0.8)
             make.height.equalTo(60)
-            make.top.equalTo(courseTextField.snp.bottom).offset(40)
+            make.bottom.equalToSuperview().offset(-100)
         }
     }
     
@@ -90,7 +93,47 @@ class AddCourseViewController: UIViewController {
                     print(error)
                 }
             courseTextField.text = ""
-            self.navigationController?.popViewController(animated: true)
+            self.navigationController?.dismiss(animated: true)
+        }
+    }
+    
+    // MARK: - PANNING temp implementation, should generalize so other vc's can easily implement
+    
+    var panGestureRecognizer: UIPanGestureRecognizer?
+    var originalPosition: CGPoint?
+    var currentPositionTouched: CGPoint?
+    
+    @objc func panGestureAction(_ panGesture: UIPanGestureRecognizer) {
+        let translation = panGesture.translation(in: view)
+        
+        if panGesture.state == .began {
+            originalPosition = view.center
+            currentPositionTouched = panGesture.location(in: view)
+        } else if panGesture.state == .changed {
+            view.frame.origin = CGPoint(
+                x: 0,
+                y: translation.y > 0 ? translation.y : 0
+            )
+        } else if panGesture.state == .ended {
+            let velocity = panGesture.velocity(in: view)
+            
+            if velocity.y >= 1500 {
+                UIView.animate(withDuration: 0.2
+                    , animations: {
+                        self.view.frame.origin = CGPoint(
+                            x: self.view.frame.origin.x,
+                            y: self.view.frame.size.height
+                        )
+                }, completion: { (isCompleted) in
+                    if isCompleted {
+                        self.dismiss(animated: false, completion: nil)
+                    }
+                })
+            } else {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.view.center = self.originalPosition!
+                })
+            }
         }
     }
 }
