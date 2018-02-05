5ad0504f5c47e3b74da8fa8399d7a21f670a43a1
Kevin Chan <kevinchan159@gmail.com>
2018-02-05 00:55:44 -0500

    setting up home screen

--- Clicker/UIColor+Shared.swift
+++ Clicker/UIColor+Shared.swift
@@ -13,8 +13,9 @@ extension UIColor {
     @nonobjc static let clickerBlue = UIColor.colorFromCode(0x4EA0FF)
     @nonobjc static let clickerBackground = UIColor(red: 247/255, green: 249/255, blue: 250/255, alpha: 1.0)
     @nonobjc static let clickerGreen = UIColor(red: 41/255, green: 192/255, blue: 158/255, alpha: 1.0)
-    @nonobjc static let clickerMediumGray = UIColor(red: 229/255, green: 231/255, blue: 237/255, alpha: 1.0)
-    @nonobjc static let clickerDarkGray = UIColor(red: 158/255, green: 167/255, blue: 179/255, alpha: 1.0)
+    @nonobjc static let clickerLightGray = UIColor(red: 229/255, green: 231/255, blue: 237/255, alpha: 1.0)
+    @nonobjc static let clickerMediumGray = UIColor(red: 158/255, green: 167/255, blue: 179/255, alpha: 1.0)
+    @nonobjc static let clickerDarkGray = UIColor(red: 122/255, green: 129/255, blue: 139/255, alpha: 1.0)
 
     public static func colorFromCode(_ code: Int) -> UIColor {
         let red = CGFloat(((code & 0xFF0000) >> 16)) / 255
