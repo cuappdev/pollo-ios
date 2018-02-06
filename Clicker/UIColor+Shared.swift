330e1dc09ae276d3160c51bb2270b5d9a14193b0
Kevin Chan <kevinchan159@gmail.com>
2018-02-05 23:09:03 -0500

    finished basic poll screen

--- Clicker/UIColor+Shared.swift
+++ Clicker/UIColor+Shared.swift
@@ -10,8 +10,9 @@ import UIKit
 
 extension UIColor {
     
-    @nonobjc static let clickerBlue = UIColor.colorFromCode(0x4EA0FF)
     @nonobjc static let clickerBackground = UIColor(red: 247/255, green: 249/255, blue: 250/255, alpha: 1.0)
+    @nonobjc static let clickerBorder = UIColor(red: 225/255, green: 225/255, blue: 225/255, alpha: 1.0)
+    @nonobjc static let clickerBlue = UIColor(red: 73/255, green: 157/255, blue: 255/255, alpha: 1.0)
     @nonobjc static let clickerGreen = UIColor(red: 41/255, green: 192/255, blue: 158/255, alpha: 1.0)
     @nonobjc static let clickerLightGray = UIColor(red: 229/255, green: 231/255, blue: 237/255, alpha: 1.0)
     @nonobjc static let clickerMediumGray = UIColor(red: 158/255, green: 167/255, blue: 179/255, alpha: 1.0)
--- Clicker/View Controllers/CreateQuestionViewController.swift
+++ Clicker/View Controllers/CreateQuestionViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 import SnapKit
 
-class CreateQuestionViewController: UIViewController {
+class CreateQuestionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
     
     var codeBarButtonItem: UIBarButtonItem!
     var endSessionBarButtonItem: UIBarButtonItem!
+    var questionOptionsView: QuestionOptionsView!
+    var questionCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +27,34 @@ class CreateQuestionViewController: UIViewController {
         setupConstraints()
     }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.item == 0 {
+            let cell = questionCollectionView.dequeueReusableCell(withReuseIdentifier: "multipleChoiceCellID", for: indexPath) as! MultipleChoiceCell
+            return cell
+        }
+        let cell = questionCollectionView.dequeueReusableCell(withReuseIdentifier: "freeResponseCellID", for: indexPath) as! FreeResponseCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: questionCollectionView.frame.width, height: questionCollectionView.frame.height)
+    }
+    
     @objc func endSession() {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        questionOptionsView.sliderBarLeftConstraint.constant = scrollView.contentOffset.x / 2
+    }
+    
+    func scrollToIndex(index: Int) {
+        let indexPath = IndexPath(item: index, section: 0)
+        questionCollectionView.scrollToItem(at: indexPath, at: [], animated: true)
     }
     
     func setupViews() {
@@ -48,10 +77,41 @@ class CreateQuestionViewController: UIViewController {
         endSessionButton.addTarget(self, action: #selector(endSession), for: .touchUpInside)
         endSessionBarButtonItem = UIBarButtonItem(customView: endSessionButton)
         self.navigationItem.rightBarButtonItem = endSessionBarButtonItem
+        
+        questionOptionsView = QuestionOptionsView(frame: .zero, options: ["Multiple Choice", "Free Response"], controller: self)
+        view.addSubview(questionOptionsView)
+        
+        let layout = UICollectionViewFlowLayout()
+        questionCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.scrollDirection = .horizontal
+        questionCollectionView.alwaysBounceHorizontal = true
+        questionCollectionView.delegate = self
+        questionCollectionView.dataSource = self
+        questionCollectionView.showsVerticalScrollIndicator = false
+        questionCollectionView.showsHorizontalScrollIndicator = false
+        questionCollectionView.register(MultipleChoiceCell.self, forCellWithReuseIdentifier: "multipleChoiceCellID")
+        questionCollectionView.register(FreeResponseCell.self, forCellWithReuseIdentifier: "freeResponseCellID")
+        questionCollectionView.backgroundColor = .clickerBackground
+        questionCollectionView.isPagingEnabled = true
+        view.addSubview(questionCollectionView)
+        
     }
     
     func setupConstraints() {
+        questionOptionsView.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: view.frame.width, height: view.frame.height * 0.06596701649))
+            make.top.equalTo(self.topLayoutGuide.snp.bottom)
+            make.left.equalToSuperview()
+        }
         
+        questionCollectionView.snp.updateConstraints { make in
+            make.width.equalToSuperview()
+            make.left.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.top.equalTo(questionOptionsView.snp.bottom)
+        }
     }
     
 }
