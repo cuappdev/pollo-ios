3f0b726c021c29543ec11f04af426b457efdf476
Kevin Chan <kevinchan159@gmail.com>
2018-02-06 22:00:55 -0500

    setup timer in live results

--- Clicker/View Controllers/LiveResultsViewController.swift
+++ Clicker/View Controllers/LiveResultsViewController.swift
@@ -11,15 +11,127 @@ import SnapKit
 
 class LiveResultsViewController: UIViewController {
     
+    var codeBarButtonItem: UIBarButtonItem!
+    var endSessionBarButtonItem: UIBarButtonItem!
+    var headerView: UIView!
+    var separatorView: UIView!
     var liveResultsLabel: UILabel!
     var timerLabel: UILabel!
     var editPollButton: UIButton!
+    var timer: Timer!
+    var elapsedSeconds: Int = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .clickerBackground
         
+        setupNavBar()
+        setupViews()
+        setupConstraints()
+        runTimer()
     }
     
+    @objc func endSession() {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTime() {
+        elapsedSeconds += 1
+        print(elapsedSeconds)
+        if (elapsedSeconds < 10) {
+            timerLabel.text = "00:0\(elapsedSeconds)"
+        } else if (elapsedSeconds < 60) {
+            timerLabel.text = "00:\(elapsedSeconds)"
+        } else {
+            var minutes = Int(elapsedSeconds / 60)
+            var seconds = elapsedSeconds - minutes * 60
+            if (elapsedSeconds < 600) {
+                timerLabel.text = "0\(minutes):\(seconds)"
+            } else {
+                timerLabel.text = "\(minutes):\(seconds)"
+            }
+        }
+    }
+    
+    func setupViews() {
+        headerView = UIView()
+        headerView.backgroundColor = .clickerBackground
+        view.addSubview(headerView)
+        
+        separatorView = UIView()
+        separatorView.backgroundColor = .clickerMediumGray
+        headerView.addSubview(separatorView)
+        
+        liveResultsLabel = UILabel()
+        liveResultsLabel.text = "Live Results"
+        liveResultsLabel.font = UIFont._16MediumFont
+        liveResultsLabel.textColor = UIColor(red: 31/255, green: 44/255, blue: 56/255, alpha: 1.0)
+        liveResultsLabel.textAlignment = .center
+        headerView.addSubview(liveResultsLabel)
+        
+        timerLabel = UILabel()
+        timerLabel.text = "00:00"
+        timerLabel.textColor = UIColor.clickerMediumGray
+        timerLabel.font = UIFont._16MediumFont
+        headerView.addSubview(timerLabel)
+
+        
+        
+    }
+    
+    func setupConstraints() {
+        headerView.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: view.frame.width, height: view.frame.height * 0.06596701649))
+            make.left.equalToSuperview()
+            make.top.equalTo(self.topLayoutGuide.snp.bottom).offset(20)
+        }
+        
+        separatorView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalTo(0.5)
+            make.left.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        
+        liveResultsLabel.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: view.frame.width * 0.25, height: 20))
+            make.left.equalToSuperview().offset(18)
+            make.centerY.equalToSuperview()
+        }
+        
+        timerLabel.snp.makeConstraints { make in
+            make.size.equalTo(CGSize(width: view.frame.width * 0.15, height: 20))
+            make.left.equalTo(liveResultsLabel.snp.right).offset(10)
+            make.centerY.equalToSuperview()
+        }
+        
+        
+    }
+    
+    func setupNavBar() {
+        let codeLabel = UILabel()
+        let codeAttributedString = NSMutableAttributedString(string: "SESSION CODE: APPDEV")
+        codeAttributedString.addAttribute(.font, value: UIFont._16RegularFont, range: NSRange(location: 0, length: 13))
+        codeAttributedString.addAttribute(.font, value: UIFont._16MediumFont, range: NSRange(location: 13, length: codeAttributedString.length - 13))
+        codeLabel.attributedText = codeAttributedString
+        codeLabel.textColor = .white
+        codeLabel.backgroundColor = .clear
+        codeBarButtonItem = UIBarButtonItem(customView: codeLabel)
+        self.navigationItem.leftBarButtonItem = codeBarButtonItem
+        
+        let endSessionButton = UIButton()
+        let endSessionAttributedString = NSMutableAttributedString(string: "End Session")
+        endSessionAttributedString.addAttribute(.font, value: UIFont._16SemiboldFont, range: NSRange(location: 0, length: endSessionAttributedString.length))
+        endSessionAttributedString.addAttribute(.foregroundColor, value: UIColor.white, range: NSRange(location: 0, length: endSessionAttributedString.length))
+        endSessionButton.setAttributedTitle(endSessionAttributedString, for: .normal)
+        endSessionButton.backgroundColor = .clear
+        endSessionButton.addTarget(self, action: #selector(endSession), for: .touchUpInside)
+        endSessionBarButtonItem = UIBarButtonItem(customView: endSessionButton)
+        self.navigationItem.rightBarButtonItem = endSessionBarButtonItem
+    }
 }
