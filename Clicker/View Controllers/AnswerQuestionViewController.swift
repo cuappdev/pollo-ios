949aabd25e11570f0d28fe35f5aaae742286f1cd
Keivan Shahida <keivanshahida@icloud.com>
2018-02-16 14:26:52 -0500

    Question answering VC basic set up.

--- Clicker/View Controllers/AnswerQuestionViewController.swift
+++ Clicker/View Controllers/AnswerQuestionViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 import SnapKit
 
-class AnswerQuestionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    
+class AnswerQuestionViewController: UIViewController {
     
     var codeBarButtonItem: UIBarButtonItem!
     var endSessionBarButtonItem: UIBarButtonItem!
-    var questionOptionsView: QuestionOptionsView!
-    var questionCollectionView: UICollectionView!
     
+    var pollCode: String!
+    
+    //MARK: - INITIALIZATION
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,83 +24,12 @@ class AnswerQuestionViewController: UIViewController, UICollectionViewDataSource
         UINavigationBar.appearance().barTintColor = .clickerGreen
         
         setupNavBar()
-        setupViews()
         setupConstraints()
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if indexPath.item == 0 {
-            let cell = questionCollectionView.dequeueReusableCell(withReuseIdentifier: "createMultipleChoiceCellID", for: indexPath) as! CreateMultipleChoiceCell
-            cell.createQuestionVC = self
-            return cell
-        }
-        let cell = questionCollectionView.dequeueReusableCell(withReuseIdentifier: "createFreeResponseCellID", for: indexPath) as! CreateFreeResponseCell
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: questionCollectionView.frame.width, height: questionCollectionView.frame.height)
-    }
-    
-    @objc func endSession() {
-        self.navigationController?.popToRootViewController(animated: true)
-    }
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        questionOptionsView.sliderBarLeftConstraint.constant = scrollView.contentOffset.x / 2
-    }
-    
-    func scrollToIndex(index: Int) {
-        let indexPath = IndexPath(item: index, section: 0)
-        questionCollectionView.scrollToItem(at: indexPath, at: [], animated: true)
-    }
-    
-    func setupViews() {
-        questionOptionsView = QuestionOptionsView(frame: .zero, options: ["Multiple Choice", "Free Response"], controller: self)
-        view.addSubview(questionOptionsView)
-        
-        let layout = UICollectionViewFlowLayout()
-        questionCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        layout.minimumInteritemSpacing = 0
-        layout.minimumLineSpacing = 0
-        layout.scrollDirection = .horizontal
-        questionCollectionView.alwaysBounceHorizontal = true
-        questionCollectionView.delegate = self
-        questionCollectionView.dataSource = self
-        questionCollectionView.showsVerticalScrollIndicator = false
-        questionCollectionView.showsHorizontalScrollIndicator = false
-        questionCollectionView.register(CreateMultipleChoiceCell.self, forCellWithReuseIdentifier: "createMultipleChoiceCellID")
-        questionCollectionView.register(CreateFreeResponseCell.self, forCellWithReuseIdentifier: "createFreeResponseCellID")
-        questionCollectionView.backgroundColor = .clickerBackground
-        questionCollectionView.isPagingEnabled = true
-        view.addSubview(questionCollectionView)
-        
-    }
-    
-    func setupConstraints() {
-        questionOptionsView.snp.makeConstraints { make in
-            make.size.equalTo(CGSize(width: view.frame.width, height: view.frame.height * 0.06596701649))
-            make.top.equalTo(self.topLayoutGuide.snp.bottom).offset(20)
-            make.left.equalToSuperview()
-        }
-        
-        questionCollectionView.snp.updateConstraints { make in
-            make.width.equalToSuperview()
-            make.left.equalToSuperview()
-            make.bottom.equalToSuperview()
-            make.top.equalTo(questionOptionsView.snp.bottom)
-        }
     }
     
     func setupNavBar() {
         let codeLabel = UILabel()
-        let pollCode = UserDefaults.standard.value(forKey: "pollCode") as! String
-        let codeAttributedString = NSMutableAttributedString(string: "SESSION CODE: \(pollCode)")
+        let codeAttributedString = NSMutableAttributedString(string: "SESSION CODE: \(pollCode ?? "------")")
         codeAttributedString.addAttribute(.font, value: UIFont._16RegularFont, range: NSRange(location: 0, length: 13))
         codeAttributedString.addAttribute(.font, value: UIFont._16MediumFont, range: NSRange(location: 13, length: codeAttributedString.length - 13))
         codeLabel.attributedText = codeAttributedString
@@ -110,7 +39,7 @@ class AnswerQuestionViewController: UIViewController, UICollectionViewDataSource
         self.navigationItem.leftBarButtonItem = codeBarButtonItem
         
         let endSessionButton = UIButton()
-        let endSessionAttributedString = NSMutableAttributedString(string: "End Session")
+        let endSessionAttributedString = NSMutableAttributedString(string: "Exit Session")
         endSessionAttributedString.addAttribute(.font, value: UIFont._16SemiboldFont, range: NSRange(location: 0, length: endSessionAttributedString.length))
         endSessionAttributedString.addAttribute(.foregroundColor, value: UIColor.white, range: NSRange(location: 0, length: endSessionAttributedString.length))
         endSessionButton.setAttributedTitle(endSessionAttributedString, for: .normal)
@@ -120,9 +49,18 @@ class AnswerQuestionViewController: UIViewController, UICollectionViewDataSource
         self.navigationItem.rightBarButtonItem = endSessionBarButtonItem
     }
     
-    // MARK: - Keyboard
+    func setupConstraints() {
+        
+    }
     
+    // MARK: - KEYBOARD
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
+    
+    // MARK: - SESSION
+    @objc func endSession() {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
 }
+
