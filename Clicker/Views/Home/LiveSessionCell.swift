5108fa246e489c62eed26c726d19ffeb1914dc76
Keivan Shahida <keivanshahida@icloud.com>
2018-02-19 18:10:29 -0500

    SOLVED the infamous header bug

--- Clicker/Views/Home/LiveSessionCell.swift
+++ Clicker/Views/Home/LiveSessionCell.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class LiveSessionCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    // MARK: - INITIALIZATION
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .clear
+        
     }
-
+    
+    // MARK: - LAYOUT
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
     }
-
+    
+    override func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
