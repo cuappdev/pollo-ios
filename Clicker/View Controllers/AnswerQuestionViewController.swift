7302ac9dd107ea46361689f4134f9e0c1da8753d
Kevin Chan <kevinchan159@gmail.com>
2018-02-27 15:21:37 -0500

    cleaned up AnswerVC

--- Clicker/View Controllers/AnswerQuestionViewController.swift
+++ Clicker/View Controllers/AnswerQuestionViewController.swift
@@ -37,28 +37,7 @@ class AnswerQuestionViewController: UIViewController, UITableViewDelegate, UITab
         setupConstraints()
     }
     
-    func setupNavBar() {
-        let codeLabel = UILabel()
-        let codeAttributedString = NSMutableAttributedString(string: "SESSION CODE: \(pollCode ?? "------")")
-        codeAttributedString.addAttribute(.font, value: UIFont._16RegularFont, range: NSRange(location: 0, length: 13))
-        codeAttributedString.addAttribute(.font, value: UIFont._16MediumFont, range: NSRange(location: 13, length: codeAttributedString.length - 13))
-        codeLabel.attributedText = codeAttributedString
-        codeLabel.textColor = .white
-        codeLabel.backgroundColor = .clear
-        codeBarButtonItem = UIBarButtonItem(customView: codeLabel)
-        self.navigationItem.leftBarButtonItem = codeBarButtonItem
-        
-        let endSessionButton = UIButton()
-        let endSessionAttributedString = NSMutableAttributedString(string: "Exit Session")
-        endSessionAttributedString.addAttribute(.font, value: UIFont._16SemiboldFont, range: NSRange(location: 0, length: endSessionAttributedString.length))
-        endSessionAttributedString.addAttribute(.foregroundColor, value: UIColor.white, range: NSRange(location: 0, length: endSessionAttributedString.length))
-        endSessionButton.setAttributedTitle(endSessionAttributedString, for: .normal)
-        endSessionButton.backgroundColor = .clear
-        endSessionButton.addTarget(self, action: #selector(endSession), for: .touchUpInside)
-        endSessionBarButtonItem = UIBarButtonItem(customView: endSessionButton)
-        self.navigationItem.rightBarButtonItem = endSessionBarButtonItem
-    }
-    
+    // MARK: - Setup/layout views
     func setupViews() {
         
         questionLabel = UILabel()
@@ -127,8 +106,29 @@ class AnswerQuestionViewController: UIViewController, UITableViewDelegate, UITab
         }
     }
     
-    // MARK: Submit button pressed
+    func setupNavBar() {
+        let codeLabel = UILabel()
+        let codeAttributedString = NSMutableAttributedString(string: "SESSION CODE: \(pollCode ?? "------")")
+        codeAttributedString.addAttribute(.font, value: UIFont._16RegularFont, range: NSRange(location: 0, length: 13))
+        codeAttributedString.addAttribute(.font, value: UIFont._16MediumFont, range: NSRange(location: 13, length: codeAttributedString.length - 13))
+        codeLabel.attributedText = codeAttributedString
+        codeLabel.textColor = .white
+        codeLabel.backgroundColor = .clear
+        codeBarButtonItem = UIBarButtonItem(customView: codeLabel)
+        self.navigationItem.leftBarButtonItem = codeBarButtonItem
+        
+        let endSessionButton = UIButton()
+        let endSessionAttributedString = NSMutableAttributedString(string: "Exit Session")
+        endSessionAttributedString.addAttribute(.font, value: UIFont._16SemiboldFont, range: NSRange(location: 0, length: endSessionAttributedString.length))
+        endSessionAttributedString.addAttribute(.foregroundColor, value: UIColor.white, range: NSRange(location: 0, length: endSessionAttributedString.length))
+        endSessionButton.setAttributedTitle(endSessionAttributedString, for: .normal)
+        endSessionButton.backgroundColor = .clear
+        endSessionButton.addTarget(self, action: #selector(endSession), for: .touchUpInside)
+        endSessionBarButtonItem = UIBarButtonItem(customView: endSessionButton)
+        self.navigationItem.rightBarButtonItem = endSessionBarButtonItem
+    }
     
+    // Submit button pressed
     @objc func submitAnswer() {
         if (selectedOptionIndex == -1) {
             return
