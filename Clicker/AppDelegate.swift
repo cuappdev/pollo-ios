859cba768c527e43a675ba5081832ee5c68acbda
Kevin Chan <kevinchan159@gmail.com>
2018-03-28 11:06:49 -0400

    created Group tab cells; added slider view

--- Clicker/AppDelegate.swift
+++ Clicker/AppDelegate.swift
@@ -20,8 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        let navigationController = UINavigationController(rootViewController: HomeViewController())
+        // let navigationController = UINavigationController(rootViewController: HomeViewController())
         // let navigationController = UINavigationController(rootViewController: GroupHomeViewController())
+        let navigationController = UINavigationController(rootViewController: GroupViewController())
         window?.rootViewController = navigationController
         
         let siren = Siren.shared
---
+++ Clicker/Views/Groups/CreatedCell.swift
@@ -0,0 +1,21 @@
+//
+//  CreatedCell.swift
+//  Clicker
+//
+//  Created by Kevin Chan on 3/28/18.
+//  Copyright © 2018 CornellAppDev. All rights reserved.
+//
+
+import UIKit
+
+class CreatedCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
