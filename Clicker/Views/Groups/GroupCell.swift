fbf7d1731343ee79f9e462194fd06aa8a5df7c2e
Kevin Chan <kevinchan159@gmail.com>
2018-04-11 12:47:35 -0400

    setup group collection view; added components to group cell

--- Clicker/Views/Groups/GroupCell.swift
+++ Clicker/Views/Groups/GroupCell.swift
@@ -9,10 +9,32 @@
 import Foundation
 import UIKit
 
-class GroupCell: UICollectionViewCell {
+class GroupCell: UITableViewCell {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    var nameLabel: UILabel!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+        layoutSubviews()
+    }
+    
+    func setupViews() {
+        nameLabel = UILabel()
+        nameLabel.font = UIFont._18RegularFont
+        addSubview(nameLabel)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        nameLabel.snp.makeConstraints { make in
+            make.width.equalTo(nameLabel.intrinsicContentSize.width)
+            make.height.equalTo(24)
+            make.top.equalToSuperview().offset(18)
+            make.left.equalToSuperview().offset(18)
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
