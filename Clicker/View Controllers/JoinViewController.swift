672419190349c4437d6b4cdf44427bef37be4579
Annie Cheng <ac962@cornell.edu>
2018-04-18 00:47:19 -0400

    Add UI for Join Session Popup

--- Clicker/View Controllers/JoinViewController.swift
+++ Clicker/View Controllers/JoinViewController.swift
@@ -7,29 +7,100 @@
 //
 import UIKit
 
-class JoinViewController: UITabBarController {
+class JoinViewController: UIViewController, UITextFieldDelegate {
+    
+    let joinButtonSize: CGSize = CGSize(width: 83.5, height: 44)
+    let codeTextFieldHeight: CGFloat = 47
+    let edgePadding: CGFloat = 18
+    let textFieldPadding: CGFloat = 1.5
+    let exitButtonHeight: CGFloat = 32
+    let bottomPadding: CGFloat = 11
+    
+    var session: Socket!
+    var dismissController: UIViewController!
+    var tapGestureRecognizer: UITapGestureRecognizer!
+    var codeTextField: UITextField!
+    var joinButton: UIButton!
+    var exitButton: UIButton!
+    var popupHeight: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Join"
-        view.backgroundColor = .clickerNavBarLightGrey
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        view.backgroundColor = .clickerDeepBlack
+
         setupViews()
-        setupConstraints()
-        // Do any additional setup after loading the view.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func setupViews() {
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGestureRecognizer)
         
+        codeTextField = UITextField(frame: CGRect(x: edgePadding, y: edgePadding, width: view.frame.size.width - edgePadding * 2, height: codeTextFieldHeight))
+        codeTextField.delegate = self
+        codeTextField.layer.cornerRadius = codeTextFieldHeight / 2
+        codeTextField.borderStyle = .none
+        codeTextField.font = ._16MediumFont
+        codeTextField.backgroundColor = .white
+        codeTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: edgePadding, height: codeTextFieldHeight))
+        codeTextField.leftViewMode = .always
+        codeTextField.attributedPlaceholder = NSAttributedString(string: "Enter a code", attributes: [NSAttributedStringKey.foregroundColor: UIColor.clickerMediumGray, NSAttributedStringKey.font: UIFont._16MediumFont])
+        view.addSubview(codeTextField)
+
+        joinButton = UIButton(frame: CGRect(x: codeTextField.frame.size.width - joinButtonSize.width - textFieldPadding, y: textFieldPadding, width: joinButtonSize.width, height: joinButtonSize.height))
+        joinButton.setTitle("Join", for: .normal)
+        joinButton.setTitleColor(.white, for: .normal)
+        joinButton.titleLabel?.font = ._16SemiboldFont
+        joinButton.titleLabel?.textAlignment = .center
+        joinButton.backgroundColor = .clickerGreen
+        joinButton.layer.cornerRadius = joinButtonSize.height / 2
+        joinButton.addTarget(self, action: #selector(join), for: .touchUpInside)
+        codeTextField.addSubview(joinButton)
+
+        exitButton = UIButton(frame: CGRect(x: 0, y: popupHeight - bottomPadding - exitButtonHeight, width: exitButtonHeight, height: exitButtonHeight))
+        exitButton.center.x = view.center.x
+        exitButton.setImage(#imageLiteral(resourceName: "ExitIcon"), for: .normal)
+        exitButton.backgroundColor = .clear
+        exitButton.addTarget(self, action: #selector(exit), for: .touchUpInside)
+        view.addSubview(exitButton)
     }
     
-    func setupConstraints() {
-        
+    @objc func join() {
+        // TODO: Join session
+        print("join button clicked")
     }
     
+    @objc func exit() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - TEXT VIEW DELEGATE
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    // MARK: - KEYBOARD
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            self.view.frame.origin.y -= keyboardSize.height
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            self.view.frame.origin.y += keyboardSize.height
+        }
+    }
+    
+    @objc func hideKeyboard() {
+        // Hide keyboard when user taps outside of text field on popup view
+        codeTextField.resignFirstResponder()
+    }
 }
--- Clicker/View Controllers/TabBarController.swift
+++ Clicker/View Controllers/TabBarController.swift
@@ -6,16 +6,23 @@
 //  Copyright © 2018 CornellAppDev. All rights reserved.
 //
 import UIKit
+import Presentr
+import SnapKit
 
 class TabBarController: UITabBarController {
     
+    let popupViewHeight: CGFloat = 140
+    
     var pollsViewController: PollsViewController!
-    var joinViewController: JoinViewController!
+    var dummyViewController: UIViewController!
     var groupViewController: GroupsViewController!
+    var joinSessionButton: UIButton!
     
     var pollsNavigationController: UINavigationController!
+    var dummyNavigationController: UINavigationController!
     var groupNavigationController: UINavigationController!
     
+    var tabBarHeight: CGFloat!
 
     // MARK: - INITIALIZATION
     override func viewDidLoad() {
@@ -23,10 +30,15 @@ class TabBarController: UITabBarController {
         print("\n\n!!!tab bar did load!!!\n\n")
         UITabBar.appearance().barTintColor = .clickerDeepBlack
         
+        tabBarHeight = tabBar.frame.size.height
+        
         setupViewControllers()
-        setupNavagationControllers()
-        let viewControllerList: [UIViewController] = [pollsNavigationController, joinViewController, groupNavigationController]
+        setupNavigationControllers()
+        
+        let viewControllerList: [UIViewController] = [pollsNavigationController, dummyNavigationController, groupNavigationController]
         viewControllers = viewControllerList
+        
+        setUpJoinSessionButton()
     }
     
     func setupViewControllers() {
@@ -36,12 +48,12 @@ class TabBarController: UITabBarController {
         pollsTabBarItem.imageInsets = UIEdgeInsets(top: 9, left: 0, bottom: -9, right: 0)
         pollsViewController.tabBarItem = pollsTabBarItem
         
-        joinViewController = JoinViewController()
-        let joinImage = UIImage(named: "join")?.withRenderingMode(.alwaysOriginal)
+        dummyViewController = UIViewController()
+        let joinImage = UIImage(named: "JoinTabBarIcon")?.withRenderingMode(.alwaysOriginal)
         let joinTabBarItem = UITabBarItem(title: "", image: joinImage, tag: 1)
         joinTabBarItem.imageInsets = UIEdgeInsets(top: 9, left: 0, bottom: -9, right: 0)
-        joinViewController.tabBarItem = joinTabBarItem
-        
+        dummyViewController.tabBarItem = joinTabBarItem
+
         groupViewController = GroupsViewController()
         let selectedGroupImage = UIImage(named: "groups_selected")?.withRenderingMode(.alwaysOriginal)
         let groupsTabBarItem = UITabBarItem(title: "", image: #imageLiteral(resourceName: "groups"), selectedImage: selectedGroupImage)
@@ -49,16 +61,54 @@ class TabBarController: UITabBarController {
         groupViewController.tabBarItem = groupsTabBarItem
     }
     
-    func setupNavagationControllers() {
+    func setupNavigationControllers() {
         pollsNavigationController = UINavigationController(rootViewController: pollsViewController)
         pollsNavigationController.setNavigationBarHidden(true, animated: false)
         pollsNavigationController.navigationBar.barTintColor = .clickerDeepBlack
         pollsNavigationController.navigationBar.isTranslucent = false
+        
+        dummyNavigationController = UINavigationController(rootViewController: dummyViewController)
+        dummyNavigationController.setNavigationBarHidden(true, animated: false)
+        dummyNavigationController.navigationBar.barTintColor = .clickerDeepBlack
+        dummyNavigationController.navigationBar.isTranslucent = false
 
         groupNavigationController = UINavigationController(rootViewController: groupViewController)
         groupNavigationController.setNavigationBarHidden(true, animated: false)
         groupNavigationController.navigationBar.barTintColor = .clickerDeepBlack
         groupNavigationController.navigationBar.isTranslucent = false
     }
-
+    
+    func setUpJoinSessionButton() {
+        joinSessionButton = UIButton()
+        joinSessionButton.backgroundColor = .clear
+        joinSessionButton.addTarget(self, action: #selector(showJoinSessionPopup), for: .touchUpInside)
+        view.addSubview(joinSessionButton)
+        
+        joinSessionButton.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.bottom.equalToSuperview()
+            make.width.equalTo(tabBarHeight)
+            make.height.equalTo(tabBarHeight)
+        }
+    }
+    
+    @objc func showJoinSessionPopup() {
+        let width = ModalSize.full
+        let height = ModalSize.custom(size: Float(popupViewHeight))
+        let originY = view.frame.height - popupViewHeight
+        let center = ModalCenterPosition.customOrigin(origin: CGPoint(x: 0, y: originY))
+        let customType = PresentationType.custom(width: width, height: height, center: center)
+        
+        let presenter: Presentr = Presentr(presentationType: customType)
+        presenter.backgroundOpacity = 0.6
+        presenter.roundCorners = false
+        presenter.dismissOnSwipe = true
+        presenter.dismissOnSwipeDirection = .bottom
+        
+        let joinSessionVC = JoinViewController()
+        joinSessionVC.dismissController = self
+        joinSessionVC.popupHeight = popupViewHeight
+        customPresentViewController(presenter, viewController: joinSessionVC, animated: true, completion: nil)
+    }
+    
 }
