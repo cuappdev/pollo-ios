927f841d2a35e83fa9ce5a22702b180e6d72e2e2
Kevin Chan <kevinchan159@gmail.com>
2018-04-18 15:42:46 -0400

    updated url to clicker-boom

--- Clicker/Supporting/Constants.swift
+++ Clicker/Supporting/Constants.swift
@@ -53,4 +53,5 @@ struct Google {
 }
 
 // let hostURL = "http://clicker-backend.cornellappdev.com"
-let hostURL = "http://localhost:3000"
+//let hostURL = "http://localhost:3000"
+let hostURL = "http://35.173.243.146"
