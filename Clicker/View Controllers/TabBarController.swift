0e82ed7287a66773298f041896a4b80dcc5b2837
Annie Cheng <ac962@cornell.edu>
2018-04-18 05:32:16 -0400

    Remove test Create Poll button from Polls screen

--- Clicker/View Controllers/TabBarController.swift
+++ Clicker/View Controllers/TabBarController.swift
@@ -99,7 +99,6 @@ class TabBarController: UITabBarController {
         let customType = PresentationType.custom(width: width, height: height, center: center)
         
         let presenter: Presentr = Presentr(presentationType: customType)
-        presenter.backgroundColor = .clickerWhite
         presenter.backgroundOpacity = 0.6
         presenter.roundCorners = false
         presenter.dismissOnSwipe = true
