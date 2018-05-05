ddfe4c852e922c5a6ae7d93713116ef4fe69df45
Kevin Chan <kevinchan159@gmail.com>
2018-05-04 23:16:59 -0400

    removed duplicate func

--- Clicker/Views/HomeScreen/Polls/PollsCell.swift
+++ Clicker/Views/HomeScreen/Polls/PollsCell.swift
@@ -28,8 +28,6 @@ class PollsCell: UICollectionViewCell, UITableViewDelegate, UITableViewDataSourc
     var sessions: [Session] = []
     var pollType: PollType!
     
-    var parentNavController: UINavigationController!
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -56,29 +54,6 @@ class PollsCell: UICollectionViewCell, UITableViewDelegate, UITableViewDataSourc
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 82.2
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let session: Session = sessions[sessions.count - indexPath.row - 1]
-        let socket = Socket(id: "\(session.id)", userType: "admin")
-        
-        switch pollType {
-        case .created:
-            let blackVC = BlackAskController()
-            blackVC.socket = socket
-            blackVC.code = session.code
-            blackVC.name = session.name
-            blackVC.sessionId = session.id
-            parentNavController.pushViewController(blackVC, animated: true)
-        default:
-            let blackVC = BlackAnswerController()
-            blackVC.socket = socket
-            blackVC.code = session.code
-            blackVC.name = session.name
-            blackVC.sessionId = session.id
-            parentNavController.pushViewController(blackVC, animated: true)
-
-        }
     }
     
     // MARK: - LAYOUT
