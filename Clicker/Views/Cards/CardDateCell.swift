c37161859420198429e6e7f74ca9b1f63950eda6
Kevin Chan <kevinchan159@gmail.com>
2018-05-06 04:09:38 -0400

    finished cardDateCell

--- Clicker/Views/Cards/CardDateCell.swift
+++ Clicker/Views/Cards/CardDateCell.swift
@@ -11,7 +11,10 @@ import UIKit
 class CardDateCell: UICollectionViewCell, CardDelegate {
     
     var userRole: UserRole!
+    var cardType: CardType!
+    var poll: Poll!
     
+    var dateLabel: UILabel!
     var shadowImage: UIImageView!
     var cardView: CardView!
     
@@ -23,15 +26,48 @@ class CardDateCell: UICollectionViewCell, CardDelegate {
     }
     
     func setupViews() {
+        dateLabel = UILabel()
+        dateLabel.textColor = .clickerMediumGray
+        dateLabel.font = ._14BoldFont
+        dateLabel.textAlignment = .center
+        addSubview(dateLabel)
+        
         shadowImage = UIImageView(image: #imageLiteral(resourceName: "cardShadow"))
         addSubview(shadowImage)
         
         cardView = CardView(frame: .zero, userRole: userRole, cardDelegate: self)
+        cardView.highlightColor = .clickerHalfGreen
         addSubview(cardView)
     }
     
     func setupConstraints() {
         
+        dateLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview()
+        }
+        
+        cardView.snp.makeConstraints { make in
+            make.top.equalTo(dateLabel.snp.bottom).offset(12)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview().inset(13)
+            make.height.equalTo(398)
+        }
+        
+        shadowImage.snp.makeConstraints { make in
+            make.width.equalTo(13)
+            make.height.equalTo(cardView.snp.height).multipliedBy(0.94)
+            make.centerY.equalTo(cardView.snp.centerY)
+            make.right.equalToSuperview()
+        }
+    }
+    
+    // MARK: Configure after variables are set
+    func configure() {
+        cardView.questionLabel.text = poll.text
+        cardView.poll = poll
+        cardView.cardType = cardType
+        cardView.setupCard()
     }
     
     // MARK: CARD DELEGATE
