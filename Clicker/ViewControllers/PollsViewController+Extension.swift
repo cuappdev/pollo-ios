e74d911439d6e69d855bdc0a0da8df53a6f8b56f
Kevin Chan <kevinchan159@gmail.com>
2018-08-27 14:32:45 -0400

    Converted home collection view to IGListKit

---
+++ Clicker/ViewControllers/PollsViewController+Extension.swift
@@ -0,0 +1,76 @@
+//
+//  PollsViewController+Extension.swift
+//  Clicker
+//
+//  Created by Kevin Chan on 8/26/18.
+//  Copyright © 2018 CornellAppDev. All rights reserved.
+//
+
+import IGListKit
+import Presentr
+import UIKit
+
+
+extension PollsViewController: ListAdapterDataSource, PollTypeSectionControllerDelegate {
+    
+    func objects(for listAdapter: ListAdapter) -> [ListDiffable] {
+        return pollTypeModels
+    }
+    
+    func listAdapter(_ listAdapter: ListAdapter, sectionControllerFor object: Any) -> ListSectionController {
+        return PollTypeSectionController(delegate: self)
+    }
+    
+    func emptyView(for listAdapter: ListAdapter) -> UIView? {
+        return nil
+    }
+    
+    // MARK: - PollTypeSectionControllerDelegate
+    var editSessionDelegate: EditSessionDelegate {
+        return self
+    }
+    
+    func sectionControllerWillDisplayPollType(sectionController: PollTypeSectionController, pollType: PollType) {
+        let item: Int = pollType == .created ? 0 : 1
+        let indexPath: IndexPath = IndexPath(item: item, section: 0)
+        pollsOptionsView.collectionView.selectItem(at: indexPath, animated: false, scrollPosition: .centeredVertically)
+    }
+}
+
+extension PollsViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pollsOptionsView.sliderBarLeftConstraint.constant = scrollView.contentOffset.x / 2
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let index = targetContentOffset.pointee.x / view.frame.width
+        let indexPath = IndexPath(item: Int(index), section: 0)
+        pollsOptionsView.collectionView.selectItem(at: indexPath, animated: true, scrollPosition: [])
+    }
+}
+
+extension PollsViewController: EditSessionDelegate {
+    func editSession(forSession session: Session) {
+        let width = ModalSize.full
+        let height = ModalSize.custom(size: editModalHeight)
+        let originY = view.frame.height - CGFloat(editModalHeight)
+        let center = ModalCenterPosition.customOrigin(origin: CGPoint(x: 0, y: originY))
+        let customType = PresentationType.custom(width: width, height: height, center: center)
+        let presenter = Presentr(presentationType: customType)
+        presenter.backgroundOpacity = 0.6
+        presenter.dismissOnSwipe = true
+        presenter.dismissOnSwipeDirection = .bottom
+        let editPollVC = EditPollViewController()
+        editPollVC.session = session
+        editPollVC.homeViewController = self
+        let navigationVC = UINavigationController(rootViewController: editPollVC)
+        customPresentViewController(presenter, viewController: navigationVC, animated: true, completion: nil)
+    }
+}
+
+extension PollsViewController: SliderBarDelegate {
+    func scrollToIndex(index: Int) {
+        let indexPath = IndexPath(item: 0, section: index)
+        pollsCollectionView.scrollToItem(at: indexPath, at: [], animated: true)
+    }
+}
