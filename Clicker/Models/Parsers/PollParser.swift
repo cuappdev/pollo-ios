4621781d6f501fa1d8b67e5ff56bc667e35a25b8
Kevin Chan <kevinchan159@gmail.com>
2018-09-13 14:11:51 -0400

    Added func to PollParser to configure PollState

--- Clicker/Models/Parsers/PollParser.swift
+++ Clicker/Models/Parsers/PollParser.swift
@@ -29,4 +29,18 @@ class PollParser: Parser {
         return Poll(id: id, text: text, questionType: questionType, options: options, results: results, state: state, answer: answer)
     }
 
+    static func parseItem(json: JSON, state: PollState) -> Poll {
+        let id = json[ParserKeys.idKey].intValue
+        let text = json[ParserKeys.textKey].stringValue
+        let options = json[ParserKeys.optionsKey].arrayValue.map { $0.stringValue }
+        let results = json[ParserKeys.resultsKey].dictionaryValue
+        let questionType: QuestionType = json[ParserKeys.typeKey].stringValue == Identifiers.multipleChoiceIdentifier
+            ? .multipleChoice
+            : .freeResponse
+        var answer: String? = nil
+        if let unwrappedAnswer = json[ParserKeys.answerKey].string, let answerDict = results[unwrappedAnswer], let answerText = answerDict[ParserKeys.textKey].string {
+            answer = answerText
+        }
+        return Poll(id: id, text: text, questionType: questionType, options: options, results: results, state: state, answer: answer)
+    }
 }
