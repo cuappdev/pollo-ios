ff0754518a20ef67fd89b36ed53179c654f38836
Jack Schluger <jes543@cornell.edu>
2018-09-13 21:15:21 -0400

    starting to implement settings screen

---
+++ Clicker/SectionControllers/SettingsSectionController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsSectionController.swift
+//  Clicker
+//
+//  Created by eoin on 9/13/18.
+//  Copyright © 2018 CornellAppDev. All rights reserved.
+//
+
+import IGListKit
+
+class SettingsSectionController: ListSectionController {
+    
+    var settingsDataModel: SettingsDataModel!
+    
+    
+
+}
