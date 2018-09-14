c9e146a20f99794ef8839c5d45ea851967f55ba6
Matthew Coufal <mjc473@cornell.edu>
2018-09-13 21:31:45 -0400

    Adjusted UI for login

--- Clicker/ViewControllers/LoginViewController.swift
+++ Clicker/ViewControllers/LoginViewController.swift
@@ -12,21 +12,51 @@ import SnapKit
 
 class LoginViewController: UIViewController, GIDSignInUIDelegate {
     
-    var appLogoImage: UIImageView!
+    var gradient: CAGradientLayer!
     var signInButton: GIDSignInButton!
+    var welcomeLabel: UILabel!
+    var appNameLabel: UILabel!
+    var pronunciationLabel: UILabel!
     
     // MARK: - INITIALIZATION
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .clickerGreen0
+        
+        gradient = CAGradientLayer()
+        gradient.colors = [UIColor.bluishGreen.cgColor, UIColor.aquaMarine.cgColor]
+        gradient.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.frame = view.bounds
+        view.layer.addSublayer(gradient)
+        
+        welcomeLabel = UILabel()
+        welcomeLabel.text = "Welcome to"
+        welcomeLabel.font = ._26MediumFont
+        welcomeLabel.textColor = .white
+        welcomeLabel.adjustsFontSizeToFitWidth = true
+        welcomeLabel.textAlignment = .center
+        view.addSubview(welcomeLabel)
+        
+        appNameLabel = UILabel()
+        appNameLabel.text = "Pollo"
+        appNameLabel.font = ._60HeavyFont
+        appNameLabel.textColor = .white
+        appNameLabel.adjustsFontSizeToFitWidth = true
+        appNameLabel.textAlignment = .center
+        view.addSubview(appNameLabel)
+        
+        pronunciationLabel = UILabel()
+        pronunciationLabel.text = "\"Poh-loh\""
+        pronunciationLabel.font = ._18MediumItalicFont
+        pronunciationLabel.textColor = .white
+        pronunciationLabel.adjustsFontSizeToFitWidth = true
+        pronunciationLabel.textAlignment = .center
+        view.addSubview(pronunciationLabel)
         
         GIDSignIn.sharedInstance().uiDelegate = self
         
-        appLogoImage = UIImageView(image: #imageLiteral(resourceName: "AppLogo"))
-        appLogoImage.contentMode = .scaleAspectFit
-        view.addSubview(appLogoImage)
-        
         signInButton = GIDSignInButton()
+        signInButton.style = .wide
         signInButton.colorScheme = .light
         view.addSubview(signInButton)
         
@@ -35,16 +65,33 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate {
     
     // MARK: - CONSTRAINTS
     func setupConstraints() {
-        appLogoImage.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.width.height.equalTo(70)
+        
+        welcomeLabel.snp.makeConstraints { make in
+            make.width.equalTo(249.5)
+            make.height.equalTo(31.5)
+            make.top.equalToSuperview().offset(194.5)
+            make.centerX.equalToSuperview()
+        }
+        
+        appNameLabel.snp.makeConstraints { make in
+            make.width.equalTo(welcomeLabel.snp.width)
+            make.height.equalTo(71.5)
+            make.top.equalTo(welcomeLabel.snp.bottom)
+            make.centerX.equalToSuperview()
+        }
+        
+        pronunciationLabel.snp.makeConstraints { make in
+            make.width.equalTo(welcomeLabel.snp.width)
+            make.height.equalTo(19)
+            make.top.equalTo(appNameLabel.snp.bottom).offset(14.5)
+            make.centerX.equalToSuperview()
         }
         
         signInButton.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().offset(-60)
+            make.width.equalTo(200)
+            make.height.equalTo(40)
+            make.top.equalTo(pronunciationLabel.snp.bottom).offset(28)
             make.centerX.equalToSuperview()
-            make.width.equalTo(view.snp.width).multipliedBy(0.3)
-            make.height.equalTo(150)
         }
         
     }
