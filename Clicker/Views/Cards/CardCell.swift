c000423a7167356f15651d80275379a6e8983fd7
Kevin Chan <kevinchan159@gmail.com>
2018-09-16 00:38:06 -0400

    Member ended FR working

--- Clicker/Views/Cards/CardCell.swift
+++ Clicker/Views/Cards/CardCell.swift
@@ -274,6 +274,7 @@ extension CardCell: FRInputSectionControllerDelegate {
                 let type: PollOptionsModelType = .frOption(optionModels: frOptionModels)
                 self.pollOptionsModel = PollOptionsModel(type: type, pollState: pollOptionsModel.pollState)
                 adapter.performUpdates(animated: false, completion: nil)
+                addFRResponseToPoll(response: response, poll: poll)
             }
             delegate.cardCellDidSubmitChoice(cardCell: self, choice: response)
         default:
@@ -293,6 +294,14 @@ extension CardCell: FRInputSectionControllerDelegate {
             return answer == frOptionModel.option
         }
         return indexOptionPair?.offset
+    }
+    
+    private func addFRResponseToPoll(response: String, poll: Poll) {
+        poll.options.append(response)
+        poll.results[response] = [
+            ParserKeys.textKey: response,
+            ParserKeys.countKey: 1
+        ]
     }
 }
 
