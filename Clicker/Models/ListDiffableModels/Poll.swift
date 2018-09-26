75540611ee17e9271959fc4160341c3c3c89f43d
Jack Schluger <jes543@cornell.edu>
2018-09-26 10:14:39 -0400

    fixing timer

--- Clicker/Models/ListDiffableModels/Poll.swift
+++ Clicker/Models/ListDiffableModels/Poll.swift
@@ -9,6 +9,10 @@
 import IGListKit
 import SwiftyJSON
 
+protocol PollTimeKeeper {
+    func startTimerWith(target aTarget: Any, selector aSelector: Selector)
+}
+
 enum PollState {
     case live
     case ended
@@ -28,10 +32,14 @@ class Poll {
     // MULTIPLE_CHOICE: {'A': {'text': 'Blue', 'count': 3}, ...}
     // FREE_RESPONSE: {'Blue': {'text': 'Blue', 'count': 3}, ...}
     
+    /// `timeLive` is how long a poll has been live for, so we can make the timer for each poll start correctly if a poll is already live when its cell is configured.
+    var timeLive: Int?
+    var timer: Timer?
+    
     // MARK: - Constants
     let identifier = UUID().uuidString
     
-    init(id: Int = -1, text: String, questionType: QuestionType, options: [String], results: [String:JSON], state: PollState, answer: String?) {
+    init(id: Int = -1, text: String, questionType: QuestionType, options: [String], results: [String:JSON], state: PollState, answer: String?, timeLive: Int? = nil) {
         self.id = id
         self.text = text
         self.questionType = questionType
@@ -39,6 +47,12 @@ class Poll {
         self.results = results
         self.state = state
         self.answer = answer
+        
+        if state == .live {
+            self.timeLive = timeLive ?? 0
+        } else {
+            self.timeLive = nil
+        }
     }
     
     // Returns array representation of results
@@ -71,5 +85,17 @@ extension Poll: ListDiffable {
         guard let object = object as? Poll else { return false }
         return id == object.id && text == object.text && questionType == object.questionType
     }
+    
+}
+
+extension Poll: PollTimeKeeper {
+    
+    func startTimerWith(target aTarget: Any, selector aSelector: Selector) {
+        if let t = timer {
+            t.invalidate()
+        }
+        timer = Timer.scheduledTimer(timeInterval: 1, target: aTarget, selector: aSelector, userInfo: nil, repeats: true)
+    }
+    
     
 }
