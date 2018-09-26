8242455810943ee48a0664d1c6c3e7eadcdb3ba7
Matthew Coufal <mjc473@cornell.edu>
2018-09-25 20:11:20 -0400

    Make drafts deletable and hide zoom out button when there are no polls

--- Clicker/ViewControllers/DraftsViewController+Extension.swift
+++ Clicker/ViewControllers/DraftsViewController+Extension.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import IGListKit
+import Presentr
 
 extension DraftsViewController: DraftCellDelegate {
     func draftCellDidSelectDraft(draft: Draft) {
@@ -33,6 +34,39 @@ extension DraftsViewController: ListAdapterDataSource {
     }
 }
 
+extension DraftsViewController: EditDraftViewControllerDelegate {
+    func editDraftViewControllerDidTapEditDraftButton(draft: Draft) {
+        delegate.draftsViewControllerShouldStartDraft(draft)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func editDraftViewControllerDidTapDeleteDraftButton(draft: Draft) {
+        DeleteDraft(id: draft.id).make()
+            .done {
+                guard let index = self.drafts.index(where: { otherDraft -> Bool in
+                    return otherDraft.id == draft.id
+                }) else { return }
+                self.drafts.remove(at: index)
+                self.adapter.performUpdates(animated: false, completion: nil)
+                
+                guard let nav = self.presentingViewController as? UINavigationController else { return }
+                guard let pollBuilderViewController = nav.topViewController as? PollBuilderViewController else { return }
+                pollBuilderViewController.getDrafts()
+            } .catch { error in
+                self.alertDeleteFailed()
+        }
+    }
+    
+    func alertDeleteFailed() {
+        let alertController = UIAlertController(title: "Error", message: "Failed to delete draft. Try again!", preferredStyle: .alert)
+        alertController.view.tintColor = .clickerGreen0
+        alertController.addAction(UIAlertAction(title: "Okay", style: .default, handler: { action in
+            alertController.dismiss(animated: true, completion: nil)
+        }))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
 extension DraftsViewController: EmptyStateCellDelegate {
     func emptyStateCellDidTapCreateDraftButton() {
         backBtnPressed()
@@ -40,8 +74,17 @@ extension DraftsViewController: EmptyStateCellDelegate {
 }
 
 extension DraftsViewController: DraftSectionControllerDelegate {
-    func draftSectionControllerDidFillDraft(draft: Draft) {
-        delegate.draftsViewControllerShouldStartDraft(draft)
-        self.dismiss(animated: true, completion: nil)
+    func draftSectionControllerDidSelectDraft(draft: Draft) {
+        let width = ModalSize.full
+        let height = ModalSize.custom(size: Float(editDraftModalSize))
+        let originY = view.bounds.height - editDraftModalSize
+        let center = ModalCenterPosition.customOrigin(origin: CGPoint(x: 0, y: originY))
+        let customType = PresentationType.custom(width: width, height: height, center: center)
+        let presenter = Presentr(presentationType: customType)
+        presenter.backgroundOpacity = 0.6
+        presenter.dismissOnSwipe = true
+        presenter.dismissOnSwipeDirection = .bottom
+        let editDraftViewController = EditDraftViewController(delegate: self, draft: draft)
+        customPresentViewController(presenter, viewController: editDraftViewController, animated: true, completion: nil)
     }
 }
