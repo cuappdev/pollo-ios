c07ad8b6331a6982db15b84f8433200f30440d1c
Jack Schluger <jes543@cornell.edu>
2018-10-14 21:49:53 -0400

    making the text on the status bar white on screens with a black background

--- Clicker/ViewControllers/CardController.swift
+++ Clicker/ViewControllers/CardController.swift
@@ -198,5 +198,9 @@ class CardController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return .lightContent
+    }
 
 }
