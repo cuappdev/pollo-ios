949894a31e6c4991149f6e641d93cd87105862b6
Jack Schluger <jes543@cornell.edu>
2018-10-16 21:25:53 -0400

    handeling correct scrolling when user touches in the middle of a scroll animation

--- Clicker/ViewControllers/CardController.swift
+++ Clicker/ViewControllers/CardController.swift
@@ -50,6 +50,7 @@ class CardController: UIViewController {
     let countLabelHorizontalPadding: CGFloat = 5
     let countLabelBackgroundViewTopPadding: CGFloat = 24
     let collectionViewHorizontalInset: CGFloat = 9.0
+    let swipeVelocityThreshold: CGFloat = 0.5
     
     init(delegate: CardControllerDelegate, pollsDateModel: PollsDateModel, session: Session, socket: Socket, userRole: UserRole, numberOfPeople: Int) {
         super.init(nibName: nil, bundle: nil)
