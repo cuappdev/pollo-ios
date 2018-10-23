2f9e3f0864e221959871c6f1183f2ae8011dcf14
Matthew Coufal <mjc473@cornell.edu>
2018-10-23 14:11:16 -0400

    Properly initialize NoResponsesCell and SpaceCell

---
+++ Clicker/SectionControllers/CardSectionControllers/QuestionSectionController 2.swift
@@ -0,0 +1,40 @@
+//
+//  QuestionSectionController.swift
+//  Clicker
+//
+//  Created by Kevin Chan on 8/31/18.
+//  Copyright © 2018 CornellAppDev. All rights reserved.
+//
+
+import IGListKit
+
+class QuestionSectionController: ListSectionController {
+    
+    // MARK: - Data vars
+    var questionModel: QuestionModel!
+    
+    // MARK: - Constants
+    let questionLabelVerticalPadding: CGFloat = 10
+    
+    // MARK: - ListSectionController overrides
+    override func sizeForItem(at index: Int) -> CGSize {
+        guard let containerSize = collectionContext?.containerSize else {
+            return .zero
+        }
+        let questionLabelWidth = containerSize.width - LayoutConstants.cardHorizontalPadding * 2
+        let cellHeight = questionModel.question.height(withConstrainedWidth: questionLabelWidth, font: ._20HeavyFont) + questionLabelVerticalPadding
+        return CGSize(width: containerSize.width, height: cellHeight)
+    }
+    
+    override func cellForItem(at index: Int) -> UICollectionViewCell {
+        let cell = collectionContext?.dequeueReusableCell(of: QuestionCell.self, for: self, at: index) as! QuestionCell
+        cell.configure(for: questionModel)
+        cell.setNeedsUpdateConstraints()
+        return cell
+    }
+    
+    override func didUpdate(to object: Any) {
+        questionModel = object as? QuestionModel
+    }
+    
+}
---
+++ Clicker/SectionControllers/SettingsSectionControllers/SettingsSectionController 2.swift
@@ -0,0 +1,65 @@
+//
+//  SettingsSectionController.swift
+//  Clicker
+//
+//  Created by eoin on 9/13/18.
+//  Copyright © 2018 CornellAppDev. All rights reserved.
+//
+
+import IGListKit
+
+class SettingsSectionController: ListSectionController {
+    
+    var settingsDataModel: SettingsDataModel!
+    
+    // MARK: Layout constants
+    let accountInfoCellHeight: CGFloat = 80.0
+    let aboutInfoCellHeight: CGFloat = 110.0
+    let feedbackInfoCellHeight: CGFloat = 130
+    let linkCellHeight: CGFloat = 39.5
+    
+    // MARK: - ListSectionController overrides
+    override func sizeForItem(at index: Int) -> CGSize {
+        guard let containerSize = collectionContext?.containerSize else {
+            return .zero
+        }
+        switch settingsDataModel.state {
+        case .info:
+            var cellHeight: CGFloat
+            switch settingsDataModel.title {
+            case "Account":
+                cellHeight = accountInfoCellHeight
+            case "About":
+                cellHeight = aboutInfoCellHeight
+            default:
+                cellHeight = feedbackInfoCellHeight
+            }
+            return CGSize(width: containerSize.width, height: cellHeight)
+        case .link:
+            return CGSize(width: containerSize.width, height: linkCellHeight)
+        }
+    }
+    
+    override func cellForItem(at index: Int) -> UICollectionViewCell {
+        guard let settingsDataModel = settingsDataModel else {
+            print("settingsDataModel not initialized")
+            return UICollectionViewCell()
+        }
+        
+        switch settingsDataModel.state {
+        case .info:
+            let cell = collectionContext?.dequeueReusableCell(of: SettingsInfoCell.self, for: self, at: index) as! SettingsInfoCell
+            cell.configureWith(settingsDataModel: settingsDataModel)
+            return cell
+        case .link:
+            let cell = collectionContext?.dequeueReusableCell(of: SettingsLinkCell.self, for: self, at: index) as! SettingsLinkCell
+            cell.configureWith(settingsDataModel: settingsDataModel)
+            return cell
+        }
+    }
+    
+    override func didUpdate(to object: Any) {
+        settingsDataModel = object as? SettingsDataModel
+    }
+
+}
