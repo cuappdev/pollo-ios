99b849a91e57607d1885a3dd77519cc4bdddadbb
Matthew Coufal <mjc473@cornell.edu>
2018-11-09 13:48:10 -0500

    Add correct answer recognition to delegates

--- Clicker/Views/PollBuilder/MCPollBuilderView.swift
+++ Clicker/Views/PollBuilder/MCPollBuilderView.swift
@@ -200,6 +200,7 @@ extension MCPollBuilderView: PollBuilderMCOptionSectionControllerDelegate {
     }
 
     func pollBuilderSectionControllerDidUpdateIsCorrect(option: String, index: Int, isCorrect: Bool) {
+        pollBuilderDelegate.updateCorrectAnswer(correctAnswer: isCorrect ? intToMCOption(index) : nil)
         if isCorrect {
             var updatedMCOptionModels: [PollBuilderMCOptionModel] = []
             mcOptionModels.enumerated().forEach { (index, mcOptionModel) in
