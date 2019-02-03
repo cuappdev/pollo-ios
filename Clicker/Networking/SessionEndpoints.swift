3b371a1011190ab62e1cb6b3b90613ca2a506094
Matthew Coufal <mjc473@cornell.edu>
2019-02-03 00:25:35 -0500

    Add rest of session endpoints

--- Clicker/Networking/SessionEndpoints.swift
+++ Clicker/Networking/SessionEndpoints.swift
@@ -38,6 +38,18 @@ extension Endpoint {
         
     }
     
+    private struct JoinSessionBody: Codable {
+        
+        var id: Int
+        var code: String
+        
+        init(id: Int, code: String) {
+            self.id = id
+            self.code = code
+        }
+        
+    }
+    
     static func generateCode() -> Endpoint {
         let headers = [
             "Authorization": "Bearer \(User.userSession?.accessToken ?? "")"
@@ -96,11 +108,84 @@ extension Endpoint {
         return Endpoint(path: "/sessions/\(id)/members", headers: headers)
     }
     
-    static func getAdmins(with id: Int) -> Endpoint {
+    static func getAdmins(with id: String) -> Endpoint {
         let headers = [
             "Authorization": "Bearer \(User.userSession?.accessToken ?? "")"
         ]
         return Endpoint(path: "/sessions/\(id)/admins", headers: headers)
+    }
+    
+    static func addMembers(id: String, memberIds: [Int]) -> Endpoint {
+        let body = [
+            "memberIds": memberIds
+        ]
+        let headers = [
+            "Authorization": "Bearer \(User.userSession?.accessToken ?? "")"
+        ]
+        return Endpoint(path: "/sessions/\(id)/members", headers: headers, body: body)
+    }
+    
+    static func removeMembers(id: String, memberIds: [Int]) -> Endpoint {
+        let body = [
+            "memberIds": memberIds
+        ]
+        let headers = [
+            "Authorization": "Bearer \(User.userSession?.accessToken ?? "")"
+        ]
+        return Endpoint(path: "/sessions/\(id)/members", headers: headers, body: body, method: EndpointMethod.delete)
+    }
+    
+    static func addAdmins(id: String, adminIds: [Int]) -> Endpoint {
+        let body = [
+            "adminIds": adminIds
+        ]
+        let headers = [
+            "Authorization": "Bearer \(User.userSession?.accessToken ?? "")"
+        ]
+        return Endpoint(path: "/sessions/\(id)/admins", headers: headers, body: body)
+    }
+    
+    static func deleteAdmins(id: String, adminIds: [Int]) -> Endpoint {
+        let body = [
+            "adminIds": adminIds
+        ]
+        let headers = [
+            "Authorization": "Bearer \(User.userSession?.accessToken ?? "")"
+        ]
+        return Endpoint(path: "/sessions/\(id)/admins", headers: headers, body: body, method: EndpointMethod.delete)
+    }
+    
+    static func startSession(code: String, name: String?, isGroup: Bool?) -> Endpoint {
+        let headers = [
+            "Authorization": "Bearer \(User.userSession?.accessToken ?? "")"
+        ]
+        if let name = name, let isGroup = isGroup {
+            let body = CreateSessionBody(name: name, code: code, isGroup: isGroup)
+            return Endpoint(path: "/start/session", headers: headers, body: body)
+        }
+        return Endpoint(path: "/start/session", headers: headers, body: ["code": code])
+    }
+    
+    static func joinSessionWithCode(with code: String) -> Endpoint {
+        let headers = [
+            "Authorization": "Bearer \(User.userSession?.accessToken ?? "")"
+        ]
+        return Endpoint(path: "/join/session", headers: headers, body: ["code": code])
+    }
+    
+    static func joinSessionWithId(with id: Int) -> Endpoint {
+        let headers = [
+            "Authorization": "Bearer \(User.userSession?.accessToken ?? "")"
+        ]
+        return Endpoint(path: "/join/session", headers: headers, body: ["id": id])
+    }
+    
+    static func joinSessionWithIdAndCode(id: Int, code: String) -> Endpoint {
+        let body = JoinSessionBody(id: id, code: code)
+        let headers = [
+            "Authorization": "Bearer \(User.userSession?.accessToken ?? "")"
+        ]
+        return Endpoint(path: "/join/session", headers: headers, body: body)
     }
     
 }
