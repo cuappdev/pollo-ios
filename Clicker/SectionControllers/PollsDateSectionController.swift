263871be9cb38838accb12b6924cd8d93489f858
Mathew Scullin <mathewscullin@dhcp-rhodes-2359.eduroam.cornell.edu>
2019-03-03 12:28:18 -0500

    Redesign layout PollsDateViewController

--- Clicker/SectionControllers/PollsDateSectionController.swift
+++ Clicker/SectionControllers/PollsDateSectionController.swift
@@ -21,16 +21,22 @@ class PollsDateSectionController: ListSectionController {
     weak var delegate: PollsDateSectionControllerDelegate?
     
     // MARK: - Constants
-    let cellHeight: CGFloat = 54
+    let cellHeight: CGFloat = 48
+    let verticalPadding: CGFloat = 16
+    let cellCornerRadius: CGFloat = 5
+    
     
     init(delegate: PollsDateSectionControllerDelegate) {
         self.delegate = delegate
+        super.init()
+        self.inset = UIEdgeInsets(top: 0, left: 0, bottom: verticalPadding, right: 0)
     }
     
     override func sizeForItem(at index: Int) -> CGSize {
-        guard let containerSize = collectionContext?.containerSize else {
+        guard let containerSize = collectionContext?.insetContainerSize else {
             return .zero
         }
+
         return CGSize(width: containerSize.width, height: cellHeight)
     }
     
@@ -38,6 +44,7 @@ class PollsDateSectionController: ListSectionController {
         let cell = collectionContext?.dequeueReusableCell(of: PollsDateCell.self, for: self, at: index) as! PollsDateCell
         cell.configure(for: pollsDateModel)
         cell.setNeedsUpdateConstraints()
+        cell.layer.cornerRadius = cellCornerRadius
         return cell
     }
     
