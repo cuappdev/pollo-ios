7e4096d2dd3c981758025bb7ec1936b340db8f6f
Jack Wilson Thompson <jwt84@cornell.edu>
2019-11-05 11:34:48 -0500

    Set initial color for join session and great group buttons and trimmed whitespace.

--- Pollo/ViewControllers/PollsViewController.swift
+++ Pollo/ViewControllers/PollsViewController.swift
@@ -169,6 +169,7 @@ class PollsViewController: UIViewController {
         createSessionButton.titleLabel?.textAlignment = .center
         createSessionButton.backgroundColor = .mediumGrey
         createSessionButton.layer.cornerRadius = codeTextFieldHeight / 2
+        createSessionButton.alpha = 0.5
         createSessionButton.addTarget(self, action: #selector(createSession), for: .touchUpInside)
         view.addSubview(createSessionButton)
 
@@ -197,6 +198,7 @@ class PollsViewController: UIViewController {
         joinSessionButton.titleLabel?.textAlignment = .center
         joinSessionButton.backgroundColor = .blueGrey
         joinSessionButton.layer.cornerRadius = codeTextFieldHeight / 2
+        joinSessionButton.alpha = 0.5
         joinSessionButton.addTarget(self, action: #selector(joinSession), for: .touchUpInside)
         view.addSubview(joinSessionButton)
         
@@ -458,13 +460,13 @@ class PollsViewController: UIViewController {
     @objc func didStartTypingCode(_ textField: UITextField) {
         if let text = textField.text {
             textField.text = text.uppercased()
-            updateJoinSessionButton(canJoin: text.count == IntegerConstants.validCodeLength)
+            updateJoinSessionButton(canJoin: text.trim().count == IntegerConstants.validCodeLength)
         }
     }
 
     @objc func didStartTypingGroupName(_ textField: UITextField) {
         if let text = textField.text {
-            updateCreateSessionButton(canCreate: text != "")
+            updateCreateSessionButton(canCreate: text.trim() != "")
         }
     }
     
