a597bc07a4c83ab96716ab20ae0a1726ef5f335e
Gonzalo Gonzalez <gg387@cornell.edu>
2020-03-29 20:42:58 -0400

    Remove pre-generated comment

--- Pollo/ViewControllers/NoInternetViewController.swift
+++ Pollo/ViewControllers/NoInternetViewController.swift
@@ -99,16 +99,4 @@ class NoInternetViewController: UIViewController {
             GIDSignIn.sharedInstance().signInSilently()
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
