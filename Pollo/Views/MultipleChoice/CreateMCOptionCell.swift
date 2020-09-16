88f16ac32b8e009ee66d8431077986b128e4669e
Gonzalo Gonzalez <gg387@cornell.edu>
2020-09-16 18:40:34 -0400

    Fix new draft bugs (#415)
    * Remove problematic code

--- Pollo/Views/MultipleChoice/CreateMCOptionCell.swift
+++ Pollo/Views/MultipleChoice/CreateMCOptionCell.swift
@@ -72,7 +72,6 @@ class CreateMCOptionCell: UICollectionViewCell, UITextFieldDelegate {
         addOptionTextField.backgroundColor = .clickerGrey6
         addOptionTextField.returnKeyType = .done
         addOptionTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
-        addOptionTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingDidEnd)
         addOptionTextField.delegate = self
         containerView.addSubview(addOptionTextField)
         
